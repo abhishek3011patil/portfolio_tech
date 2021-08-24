14b5277480cf834bd6c5fcbb8ef90bdccc84b1f4
unknown <abhishek3011patil@gmail.com>
2021-08-24 17:32:51 +0530

    home page done

---
+++ app.js
@@ -0,0 +1,24 @@
+const texts = ['Welcome To My Website !'];
+let count=  0;
+let index =0;
+let currentText = '';
+let letter = '';
+
+(function type(){
+
+    if(count === texts.length){
+        count = 0;
+    }
+
+   currentText = texts[count];
+    
+    letter = currentText.slice(0,++index);
+ document.querySelector('.typing').textContent=letter;
+
+    if(letter.length === currentText.length){
+        count++;
+       
+    }
+    setTimeout(type,100)
+
+}());
